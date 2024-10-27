1034706ca64a13b4103f323f6f5ba3046fa824b5
ren <raenierquiambao@gmail.com>
2024-10-27 22:34:51 +0800

    Make all tests passing and validate title presence

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -12,116 +12,116 @@ require 'rails_helper'
 # of tools you can use to make these specs even more expressive, but we're
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
-RSpec.describe "/tasks", type: :request do
+RSpec.describe '/tasks', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Task. As you add validations to Task, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) do
+    { title: 'MyString' }
+  end
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) do
+    { title: nil }
+  end
 
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # TasksController, or in your router and rack
   # middleware. Be sure to keep this updated too.
-  let(:valid_headers) {
+  let(:valid_headers) do
     {}
-  }
+  end
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Task.create! valid_attributes
       get tasks_url, headers: valid_headers, as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       task = Task.create! valid_attributes
       get task_url(task), as: :json
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Task" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Task' do
+        expect do
           post tasks_url,
                params: { task: valid_attributes }, headers: valid_headers, as: :json
-        }.to change(Task, :count).by(1)
+        end.to change(Task, :count).by(1)
       end
 
-      it "renders a JSON response with the new task" do
+      it 'renders a JSON response with the new task' do
         post tasks_url,
              params: { task: valid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:created)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Task" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Task' do
+        expect do
           post tasks_url,
                params: { task: invalid_attributes }, as: :json
-        }.to change(Task, :count).by(0)
+        end.to change(Task, :count).by(0)
       end
 
-      it "renders a JSON response with errors for the new task" do
+      it 'renders a JSON response with errors for the new task' do
         post tasks_url,
              params: { task: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { title: 'updated' }
+      end
 
-      it "updates the requested task" do
+      it 'updates the requested task' do
         task = Task.create! valid_attributes
         patch task_url(task),
               params: { task: new_attributes }, headers: valid_headers, as: :json
         task.reload
-        skip("Add assertions for updated state")
+        expect(task.title).to eq('updated')
       end
 
-      it "renders a JSON response with the task" do
+      it 'renders a JSON response with the task' do
         task = Task.create! valid_attributes
         patch task_url(task),
               params: { task: new_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a JSON response with errors for the task" do
+    context 'with invalid parameters' do
+      it 'renders a JSON response with errors for the task' do
         task = Task.create! valid_attributes
         patch task_url(task),
               params: { task: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including("application/json"))
+        expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested task" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested task' do
       task = Task.create! valid_attributes
-      expect {
+      expect do
         delete task_url(task), headers: valid_headers, as: :json
-      }.to change(Task, :count).by(-1)
+      end.to change(Task, :count).by(-1)
     end
   end
 end
